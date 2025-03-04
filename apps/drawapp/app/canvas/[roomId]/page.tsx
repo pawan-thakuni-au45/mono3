02df88619abd97f82ed6424aa001ad04f41ecc35
pawan <thakunipawan@gmail.com>
2025-03-04 08:57:04 +0530

    commit

--- apps/drawapp/app/canvas/[roomId]/page.tsx
+++ apps/drawapp/app/canvas/[roomId]/page.tsx
@@ -14,5 +14,8 @@ export default function Canvas(){
     },[canvasRef])
     return<div className="bg-white">
         <canvas ref={canvasRef} width={2000} height={1000}></canvas>
+        <button className="absolute bg-amber-500 bottom-0 right-0">rect</button>
+        <button className="absolute bg-amber-500 bottom-1 right-1">circle</button>
+
         </div>
 }
--- apps/drawapp/draw/index.ts
+++ apps/drawapp/draw/index.ts
@@ -1,6 +1,20 @@
+type Shape={
+    type:"rect",
+    x:number,
+    y:number,
+    height:number,
+    width:number
 
+} |{
+    type:"cicle",
+    centerX:number,
+    centerY:number,
+    radius:number
+}
 export function  initDraw(canvas:HTMLCanvasElement){
     const ctx=canvas.getContext("2d")
+
+    const existingShapes:Shape[]=[]
 
     if(!ctx){
         return
@@ -21,21 +35,41 @@ export function  initDraw(canvas:HTMLCanvasElement){
     
     canvas.addEventListener("mouseup",(e)=>{
         clicked=false
-        console.log(e.clientX);
-        console.log(e.clientY);
+        const width= e.clientX-startX;
+           const height=e.clientY-startY;
+        //here the mouse will stop making shape and then only i will push shapwes inside this array
+        existingShapes.push({
+            type:"rect",
+            x:startX,
+            y:startY,
+            height,
+            width
+
+        })
     })
     canvas.addEventListener("mousemove",(e)=>{
         if(clicked){
            const width= e.clientX-startX;
            const height=e.clientY-startY;
           
-           ctx.clearRect(0,0,canvas.width,canvas.height);
-           ctx.fillStyle= "rgba(0, 0, 0)"
-           ctx.fillRect(0, 0,canvas.width,canvas.height)
+          clearCanvas(existingShapes,canvas,ctx)
            ctx.strokeStyle="rgba(255,255,255)"
            ctx.strokeRect(startX,startY,width,height)
         }
     })
 
 
+}
+//here i am clearing rect and rendering new ones
+function clearCanvas(existingShapes:Shape[], canvas:HTMLCanvasElement,ctx:CanvasRenderingContext2D){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle= "rgba(0, 0, 0)"
+    ctx.fillRect(0, 0,canvas.width,canvas.height)
+
+    existingShapes.map((shape)=>{
+        if(shape.type=="rect"){
+            ctx.strokeStyle="rgba(255,255,255)"
+            ctx.strokeRect(shape.x,shape.y,shape.width,shape.height);
+        }
+    })
 }
